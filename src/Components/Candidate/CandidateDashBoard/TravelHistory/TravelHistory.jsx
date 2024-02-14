fc038d2ed6852dddf4b7a6af36dd46ac04755232
kowshik-7 <127116685+kowshik-7@users.noreply.github.com>
2024-02-14 23:36:09 +0530

    vedio resume

--- src/Components/Candidate/CandidateDashBoard/TravelHistory/TravelHistory.jsx
+++ src/Components/Candidate/CandidateDashBoard/TravelHistory/TravelHistory.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import "./TravelHistory.css";
+import user from "../../../../assests/User.svg";
+import dropDown from "../../../../assests/arrowDown.svg";
+import edit from "../../../../assests/edit.svg";
+import dropUp from "../../../../assests/arrowUp.svg";
+import star from "../../../../assests/star.svg";
 
 const TravelHistory = () => {
-  return <div>TravelHistory</div>;
+  const [isArrow, setIsArrow] = useState(false);
+  const dropDownhandler = () => {
+    setIsArrow(!isArrow);
+  };
+  return (
+    <div>
+      <div className="travelHistory">
+        <div className="innerTravelHistory">
+          <div
+            className={isArrow === true ? "travelHistoryHead" : "bottomBorder"}
+          >
+            <div className="travelHistoryHeadLeft">
+              <img src={user} alt="" />
+              <h1>Travel History</h1>
+            </div>
+            <div className="travelHistoryLeftIcon">
+              <img className="travelHistoryLeftIconSvg" src={edit} alt="" />
+              {isArrow === true ? (
+                <img onClick={dropDownhandler} src={dropUp} alt="" />
+              ) : (
+                <img onClick={dropDownhandler} src={dropDown} alt="" />
+              )}
+            </div>
+          </div>
+          {isArrow === true && (
+            <div className="travelHistoryDesc">
+              <h1>
+                Add certification / course Details here to enhance your profile
+              </h1>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TravelHistory;
