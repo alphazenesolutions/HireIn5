0873f9da3e07356019c684e5c26446fc7d8a94b7
kowshik-7 <127116685+kowshik-7@users.noreply.github.com>
2024-02-20 02:25:08 +0530

    userid conflitpush

--- src/Components/Login/ClientLogin/ClientLogin.jsx
+++ src/Components/Login/ClientLogin/ClientLogin.jsx
@@ -72,7 +72,7 @@ const ClientLogin = () => {
         });
       if (loginuser.access !== undefined) {
         dispatch(storeAction.tokenHandler({ token: loginuser.access }));
-        dispatch(storeAction.useridHandler({ userid: 4 }));
+        dispatch(storeAction.useridHandler({ userid: 11 }));
         dispatch(storeAction.isloginHandler({ islogin: true }));
         navigate("/discover");
       } else {
