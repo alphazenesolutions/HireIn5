5bcff86da1569cf9dc0fb362f75751a830118e2f
Dinesh <dineshkit15@gmail.com>
2024-03-01 13:31:56 +0530

    update publick

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { Store } from "./Store/Store";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-    <Provider store={Store}>
-      <App />
-    </Provider>
-  </HashRouter>
+  <Provider store={Store}>
+    <App />
+  </Provider>
 );
 reportWebVitals();
