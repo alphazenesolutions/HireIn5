0f6869dd82dfe47bb0b81d20655a51b1d9958d43
Dinesh <dineshkit15@gmail.com>
2024-03-26 10:44:18 +0530

    update admin home

--- src/Components/Client/ClientScreen/DiscoverComp/DIscoverComp.jsx
+++ src/Components/Client/ClientScreen/DiscoverComp/DIscoverComp.jsx
@@ -267,7 +267,7 @@ const DiscoverComp = () => {
   const getSearchuser = async () => {
     if (search_user.length !== 0) {
       setsearchuser(search_user);
-      dispatch(storeAction.searchuserHander({ searchuser: search_user }));
+      // dispatch(storeAction.searchuserHander({ searchuser: search_user }));
       var allsearchfacility = await axios
         .get(
           `${process.env.REACT_APP_LOCAL_HOST_URL}/user/recentlyvisited/${userid}`,
@@ -311,7 +311,7 @@ const DiscoverComp = () => {
           });
         if (alluserdata.length !== 0) {
           setsearchuser(alluserdata);
-          dispatch(storeAction.searchuserHander({ searchuser: alluserdata }));
+          // dispatch(storeAction.searchuserHander({ searchuser: alluserdata }));
         }
       } else {
         setsearchuser([]);
@@ -360,7 +360,7 @@ const DiscoverComp = () => {
           });
         if (alluserdata.length !== 0) {
           setsearchuser(alluserdata);
-          dispatch(storeAction.searchuserHander({ searchuser: alluserdata }));
+          // dispatch(storeAction.searchuserHander({ searchuser: alluserdata }));
         }
       } else {
         setsearchuser([]);
